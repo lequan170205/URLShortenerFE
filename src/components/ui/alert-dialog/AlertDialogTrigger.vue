b8b235e23325782ecf05fb8241ea13cc2c9544e8
lequan170205 <lequan170205@gmail.com>
2025-11-06 17:14:13 +0700

    init tailwind, shadcn ui

---
+++ src/components/ui/alert-dialog/AlertDialogTrigger.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { AlertDialogTrigger } from "reka-ui";
+
+const props = defineProps();
+</script>
+
+<template>
+  <AlertDialogTrigger v-bind="props">
+    <slot />
+  </AlertDialogTrigger>
+</template>
